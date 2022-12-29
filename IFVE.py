9373fb3b0a0d434fdd4c75bec43d4d593ccb92db
jvkap <jvkasouza@gmail.com>
2022-12-28 23:36:22 -0300

    Updated

--- IFVE.py
+++ IFVE.py
@@ -75,24 +75,68 @@ class MainWindow(QWidget):
                 # Get the video duration in minutes
                 duration = int(video.get(cv2.CAP_PROP_FRAME_COUNT)) / video.get(cv2.CAP_PROP_FPS) / 60
                 print(f'Duration: {duration}')
-                # Ignore the first 10 and last 10 minutes if the video is longer than 60 minutes
+                # Dont choose the frames of the first 10 and last 10 minutes if the video is longer than 60 minutes
                 if duration > 60:
-                    duration = duration - 20
-                    video.set(cv2.CAP_PROP_POS_MSEC, 600000)
-                    print(f'New duration: {duration}')
-                # Extract and save the images
+                    start_time = 10
+                    end_time = duration - 10
+                    new_duration = end_time - start_time
+                    print(f'New duration: {new_duration}')
+                else:
+                    start_time = 0
+                    end_time = duration
                 for i in range(self.num_images):
                     # Generate a random frame number between 0 and the duration of the video in seconds multiplied by the video FPS and make them proportionally spaced apart. Generate a new frame if the frame is already taken or if the frame are too close to each other.
-                    frame_num = random.randint(0, int(duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                    if duration > 60:
+                        frame_num = random.randint(0, int(new_duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                        if i == self.num_images - 1:
+                            frame_num = random.randint(0, int(new_duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                    else:
+                        frame_num = random.randint(0, int(duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                        if i == self.num_images - 1:
+                            frame_num = random.randint(0, int(duration * 60 * video.get(cv2.CAP_PROP_FPS)))
                     print(frame_num)
                     # Set the video position to the selected frame
                     video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
-                
-                    # Read and save the image with the filename of the video name and the frame number.
                     success, image = video.read()
                     if success:
+                        frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+                        print(f'frame_width: {frame_width}')
+                        frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        print(f'frame_height: {frame_height}')
+                        center_x = frame_width // 2
+                        center_y = frame_height // 2
+                        square_size = min(frame_width, frame_height)
+                        cropped_frame = image[center_y - square_size // 2:center_y + square_size // 2, center_x - square_size // 2:center_x + square_size // 2]
+                        #Check with opencv if the image are blurred.
+                        gray = cv2.cvtColor(cropped_frame, cv2.COLOR_BGR2GRAY)
+                        fm = cv2.Laplacian(gray, cv2.CV_64F).var()
+                        print(f'fm: {fm}')
+                        if fm < 80:
+                            # If the image is blurred, generate a new frame.
+                            print(f'Blurred image, generating a new frame...')
+                            if duration > 60:
+                                frame_num = random.randint(0, int(new_duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                                if i == self.num_images - 1:
+                                    frame_num = random.randint(0, int(new_duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                            else:
+                                frame_num = random.randint(0, int(duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                                if i == self.num_images - 1:
+                                    frame_num = random.randint(0, int(duration * 60 * video.get(cv2.CAP_PROP_FPS)))
+                            video.set(cv2.CAP_PROP_POS_FRAMES, frame_num)
+                            success, image = video.read()
+                            if success:
+                                frame_width = int(video.get(cv2.CAP_PROP_FRAME_WIDTH))
+                                print(f'frame_width: {frame_width}')
+                                frame_height = int(video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                                print(f'frame_height: {frame_height}')
+                                center_x = frame_width // 2
+                                center_y = frame_height // 2
+                                square_size = min(frame_width, frame_height)
+                                cropped_frame = image[center_y - square_size // 2:center_y + square_size // 2, center_x - square_size // 2:center_x + square_size // 2]
+
+                        # Save the image.
+                        cv2.imwrite(f'{self.selected_save_folder}/{self.selected_video.split("/")[-1].split(".")[0]}_{frame_num}.png', cropped_frame)
                         print(f'Saving image {i+1}...')
-                        result = cv2.imwrite(f'{self.selected_save_folder}/{self.selected_video.split("/")[-1].split(".")[0]}_{frame_num}.png', image)
                         self.progress_bar.setValue(i+1)
                     # Check the resize option and resize the image if needed.
                     if self.resize_options.currentText() == 'Resize to 512x512':
@@ -105,7 +149,7 @@ class MainWindow(QWidget):
                         image = Image.open(f'{self.selected_save_folder}/{self.selected_video.split("/")[-1].split(".")[0]}_{frame_num}.png')
                         image = image.resize((768, 768), Image.Resampling.LANCZOS)
                         image.save(f'{self.selected_save_folder}/{self.selected_video.split("/")[-1].split(".")[0]}_{frame_num}.png')
-                QMessageBox.information(self, 'Success', 'Extraction finished')
+                QMessageBox.information(self, 'Success', 'Extraction finished. The images were saved in the selected folder.')
             else:
                 QMessageBox.warning(self, 'Error', 'Please select a video and a folder to save images')
         except ValueError:
@@ -117,5 +161,4 @@ if __name__ == '__main__':
     main_window = MainWindow()
     main_window.show()
     sys.exit(app.exec_())
-
 
